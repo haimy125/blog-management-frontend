f07822c89b24b505e663fbfb762017bd74ca6ff6
myph <haimyphan125@gmail.com>
2024-10-03 11:52:12 +0700

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+
+
+// nhu nay cho m de xai
+// duong di la 
+// _app -> inde
